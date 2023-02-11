ec13be4cb62cfac2ef84da73b60782486ec64258
Basirahamnov63 <113968574+Brzrar@users.noreply.github.com>
2023-02-11 11:33:56 +0600

    Сделал hw3

---
+++ hw3/app.js
@@ -0,0 +1,54 @@
+
+
+// 1 dz
+
+// function findSmallestNumber(a, b) {
+//   return (a < b) ? a : b;
+// }
+//
+// let result = findSmallestNumber(50, 25);
+// console.log(result);
+//
+//
+
+
+
+
+
+
+// 2 dz
+
+//
+// function getStringLength(str) {
+//   return str.length;
+// }
+//
+// let result = prompt("Введите cтроки:");
+// console.log("\n" +
+//     "Длина строки: " + getStringLength(result));
+//
+
+
+
+// 3 dz
+
+// function calculator(a, b, operator) {
+//   switch (operator) {
+//     case '+':
+//       return a + b;
+//     case '-':
+//       return a - b;
+//     case '*':
+//       return a * b;
+//     case '/':
+//       if (b === 0) {
+//         return "Нельзя делить на ноль";
+//       } else {
+//         return a / b;
+//       }
+//     default:
+//       return "Недопустимый оператор";
+//   }
+// }
+//
+// console.log(calculator(10, 5, '+'));
