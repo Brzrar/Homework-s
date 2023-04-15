9b22b0313ff612b15dfa4cd650128822cfd8745b
Basirahamnov63 <113968574+Brzrar@users.noreply.github.com>
2023-04-15 22:12:04 +0600

    Update app.js

--- hw2/app.js
+++ hw2/app.js
@@ -2,42 +2,42 @@
 
 // 1
 
-// const nums = [10, 20, 30, 50, 235, 3000];
-//
-// nums.forEach(num => {
-//   if (String(num).startsWith("1") || String(num).startsWith("2") || String(num).startsWith("5")) {
-//     console.log(num);
-//   }
-// });
+const nums = [10, 20, 30, 50, 235, 3000];
+
+nums.forEach(num => {
+  if (String(num).startsWith("1") || String(num).startsWith("2") || String(num).startsWith("5")) {
+    console.log(num);
+  }
+});
 
 
 // 2
 
-// for (let num = 20; num >=0; num--) {
-//     console.log(num)
-// }
+for (let num = 20; num >=0; num--) {
+    console.log(num)
+}
 
 
 // 3
 
-// const trafficLight = {
-//   red: 'стоп',
-//   yellow: 'приготовьтесь',
-//   green: 'вперед'
-// };
-//
-// const color = prompt("Введите цвет светофора: (red, yellow, green)").toLowerCase();
-//
-// switch (color) {
-//   case 'red':
-//     alert(trafficLight.red);
-//     break;
-//   case 'yellow':
-//     alert(trafficLight.yellow);
-//     break;
-//   case 'green':
-//     alert(trafficLight.green);
-//     break;
-//   default:
-//     alert("Недопустимый цвет. Пожалуйста входите: red, yellow, or green.");
-// }
+const trafficLight = {
+  red: 'стоп',
+  yellow: 'приготовьтесь',
+  green: 'вперед'
+};
+
+const color = prompt("Введите цвет светофора: (red, yellow, green)").toLowerCase();
+
+switch (color) {
+  case 'red':
+    alert(trafficLight.red);
+    break;
+  case 'yellow':
+    alert(trafficLight.yellow);
+    break;
+  case 'green':
+    alert(trafficLight.green);
+    break;
+  default:
+    alert("Недопустимый цвет. Пожалуйста входите: red, yellow, or green.");
+}
