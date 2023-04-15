6e946c2f288e561574fe5b79201f1f1127cf3f5f
Basirahamnov63 <113968574+Brzrar@users.noreply.github.com>
2023-04-15 22:12:34 +0600

    Update app.js

--- hw3/app.js
+++ hw3/app.js
@@ -2,14 +2,14 @@
 
 // 1 dz
 
-// function findSmallestNumber(a, b) {
-//   return (a < b) ? a : b;
-// }
-//
-// let result = findSmallestNumber(50, 25);
-// console.log(result);
-//
-//
+function findSmallestNumber(a, b) {
+  return (a < b) ? a : b;
+}
+
+let result = findSmallestNumber(50, 25);
+console.log(result);
+
+
 
 
 
@@ -18,37 +18,37 @@
 
 // 2 dz
 
-//
-// function getStringLength(str) {
-//   return str.length;
-// }
-//
-// let result = prompt("Введите cтроки:");
-// console.log("\n" +
-//     "Длина строки: " + getStringLength(result));
-//
+
+function getStringLength(str) {
+  return str.length;
+}
+
+let result = prompt("Введите cтроки:");
+console.log("\n" +
+    "Длина строки: " + getStringLength(result));
+
 
 
 
 // 3 dz
 
-// function calculator(a, b, operator) {
-//   switch (operator) {
-//     case '+':
-//       return a + b;
-//     case '-':
-//       return a - b;
-//     case '*':
-//       return a * b;
-//     case '/':
-//       if (b === 0) {
-//         return "Нельзя делить на ноль";
-//       } else {
-//         return a / b;
-//       }
-//     default:
-//       return "Недопустимый оператор";
-//   }
-// }
-//
-// console.log(calculator(10, 5, '+'));
+function calculator(a, b, operator) {
+  switch (operator) {
+    case '+':
+      return a + b;
+    case '-':
+      return a - b;
+    case '*':
+      return a * b;
+    case '/':
+      if (b === 0) {
+        return "Нельзя делить на ноль";
+      } else {
+        return a / b;
+      }
+    default:
+      return "Недопустимый оператор";
+  }
+}
+
+console.log(calculator(10, 5, '+'));
